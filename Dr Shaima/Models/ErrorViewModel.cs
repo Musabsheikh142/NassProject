fb1ade2987e475bc4c9c8b6c56d739cf014278b6
Musab Sheikh <Musab Sheikh@MUSAB-SHEIKH>
2023-02-26 19:21:17 +0530

    Add project files.

---
+++ Dr Shaima/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dr_Shaima.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
